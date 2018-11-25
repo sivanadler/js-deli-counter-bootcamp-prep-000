0573a89674fc0a9bb6114f855e8215d26e030195
sadler2007 <sadler2007@gmail.com>
2018-11-25 17:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
   }
 
+  console.log("Currently serving ${line.shift()")
   return `Currently serving ${line.shift()}.`
 }
 

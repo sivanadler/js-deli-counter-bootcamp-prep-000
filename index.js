fe1f731ca2ca7095b5574c9bbd94e87e089e742a
sadler2007 <sadler2007@gmail.com>
2018-11-25 17:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function takeANumber(line, name){
   line.push(name);
   
   
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.")
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
 

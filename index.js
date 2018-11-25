e150c90b5195ff5c941434b55761e1350b074b9f
sadler2007 <sadler2007@gmail.com>
2018-11-25 17:34:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var katzDeliLine = [];
 function takeANumber(line, name){
   line.push(name);
   
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.")
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
 
